acc342b7b4dd8b6bad5eef257a9661c42021c4f6
Promise Johnson <chiemelapromise30@gmail.com>
2021-05-07 05:09:26 +1300

    Test for the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,4 +26,20 @@ RSpec.describe User, type: :model do
     expect(subject).to_not be_valid
   end
 
+  it "should not have the same name with a user" do
+    another_user = described_class.new(name: "anything", login_id: "12333")
+    subject.save
+    expect(another_user).to_not be_valid #because there is another name of "anything on the database"
+  end
+
+  it "should not accept a number for the name" do
+    subject.name = "123434"
+    expect(subject).to_not be_valid
+  end
+
+  it "should not accept letters for the login_id" do
+    subject.login_id = 'abcde'
+    expect(subject).to_not be_valid
+  end
+
 end
