41d5a1e0d5c4d47f7b19a1760890da050891cf4b
Fernando R P Marques <frpm1001@gmail.com>
2021-05-06 13:43:36 -0300

    Add rspec testing for user_event

--- spec/models/user_event_spec.rb
+++ spec/models/user_event_spec.rb
@@ -1,7 +1,28 @@
 require "rails_helper"
 
-class UserEventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+RSpec.describe UserEvent, type: :model do
+  
+  subject {
+    User.create!(name: "myuser",login_id: "12345")
+    Event.create!(event_name: "newevent", 
+      event_location: "At the hall", 
+      event_date: Date.parse('2020-7-27'),
+       hoster_id: 1)
+    described_class.new(user_id: 1, event_id: 1)
+  }
+
+  it "is valid and has valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid because it has no user_id" do
+    subject.user_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid because it has no event_id" do
+    subject.event_id = nil
+    expect(subject).to_not be_valid
+  end
+
 end
