66cb83ba8236b0640ce63af04b0842e436ce68c8
Promise Johnson <chiemelapromise30@gmail.com>
2021-05-07 05:26:37 +1300

    Add the test for the event model

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,7 +1,37 @@
 require "rails_helper"
 
-class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+RSpec.describe Event, type: :model do
+  subject {
+    described_class.new(event_name: "party and party", 
+      event_location: "At the hall", 
+      event_date: Date.parse('2020-7-27'),
+       hoster_id: 1)
+  }
+
+  it "name, date and location must be present" do
+    subject.event_name = ''
+    subject.event_location = ''
+    subject.event_date = ''
+    expect(subject).to_not be_valid
+  end
+  
+  it "event name has to be unique" do
+    subject.save
+    another_event = described_class.new(event_name: "party and party", 
+      event_location: "At the church", 
+      event_date: Date.parse('2010-7-27'),
+       hoster_id: 2)
+    expect(another_event).to_not be_valid
+  end
+
+  it "is not valid with a name shorter than 4" do
+    subject.event_name = 'nap'
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a location shorter than 4" do
+    subject.event_location = 'cap'
+    expect(subject).to_not be_valid
+  end
+
+end  
